2862a1355db2da6ba9399920a579dff4133deb11
vince <vincenthe3231@gmail.com>
2025-06-06 00:20:17 +0800

    Add profile_setup.html and update UserProfile model
    User and UserProfile are now displayed correctly in profile_setup.html. Login and Signup views still need to be implemented to complete the full flow.

--- users/models.py
+++ users/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
 class UserProfile(models.Model):
-    # user_id = models.BigIntegerField(primary_key=True)  # Explicitly set user_id as primary key
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    phone = PhoneNumberField(unique=False, blank=True, null=True)
     profession = models.CharField(max_length=40)
-    work_link = models.TextField()
-    skills = models.TextField(help_text="Comma-separated skills")
-    # email = models.EmailField(max_length=30, unique=True) # Enforce uniqueness
-    # password = models.CharField(max_length=30)
+    work_link = models.URLField(max_length=200, help_text="Link to portfolio or LinkedIn")
+    skills = models.TextField(help_text="Comma-separated skills (e.g. Python, Django, REST)")
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.first_name + ' ' + self.user.last_name}'s Profile"
--- users/views.py
+++ users/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from users.models import UserProfile
 from django.contrib.auth.models import User
 
 # Create your views here.
 class UserProfileView:
     def user_profile(self, request):
-        users = User.objects.all()
-        return render(request, "user_profile.html", {'users': users})
+        # profile = UserProfile.objects.get(user=request.user)
+        profiles = UserProfile.objects.all()
+        users = User.objects.get(id=2)
+        return render(request, "user_profile.html", {'profiles': profiles, 'users': users})
+
+    # @login_required
+    def profile_setup(self, request):
+        if request.method == "POST":
+            first_name = request.POST.get("first_name")
+            last_name = request.POST.get("last_name")
+            profession = request.POST.get("profession")
+            work_link = request.POST.get("work_link")
+            skills = request.POST.get("skills")
+
+            self.create_profile(request, first_name, last_name, profession, work_link, skills)
+        return render(request, "profile_setup.html")
+
+    def create_profile(self, first_name, last_name, profession, work_link, skills):
+        # Save the user profile information
+            user_profile = UserProfile.objects.create(
+                # user=request.user,
+                first_name=first_name,
+                last_name=last_name,
+                profession=profession,
+                work_link=work_link,
+                skills=skills
+            )
+            user_profile.save()
+            return redirect("/")
+        # return render(request, "profile_setup.html")
